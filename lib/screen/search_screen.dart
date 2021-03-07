f3f67532e1aa52f9b3789e8ee7b7eb811c58e31e
sanha kang <ncsana@gmail.com>
2021-03-07 20:59:15 +0900

    feat: 임시 와인 상세 화면

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -85,7 +85,7 @@ class _SearchScreenState extends State<SearchScreen> {
               isThreeLine: false,
               subtitle: Text(wine_item.wine_name),
               onTap: () {
-                // Navigator.of(context).push(MaterialPageRoute(builder: (context) => Detail()));
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => Detail()));
               },
             ),
           );
@@ -97,8 +97,24 @@ class _SearchScreenState extends State<SearchScreen> {
 
   }
 
-
-
-
 }
 
+
+class Detail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            Text("Detail"),
+          ],
+        ),
+      ),
+    );
+  }
+}
