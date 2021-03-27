490fd33aa48cd4855fede2bcb29f7bb46616cb51
sanha kang <ncsana@gmail.com>
2021-03-27 23:26:31 +0900

    init search_bar_2

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -11,136 +11,78 @@ import 'package:wine/widget/wine_detail.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 
-
 class SearchScreen extends StatefulWidget {
+  @override
   _SearchScreenState createState() => _SearchScreenState();
 }
 
+
 class _SearchScreenState extends State<SearchScreen> {
-  final SearchBarController<Wine> _searchBarController = SearchBarController();
-  bool isReplay = false;
-
-
-  Future<List<Wine>> _searchWineByKeyword(String search_text) async {
-    List<Wine> wines = [];
-    print('START');
-    var url =  "http://ec2-13-124-23-131.ap-northeast-2.compute.amazonaws.com:8080/api/product/search/$search_text";
-    print(url);
-    var response;
-    try {
-      response = await http.get(
-      // Uri.encodeFull("https://9l885hmyfg.execute-api.ap-northeast-2.amazonaws.com/dev/wine"),
-          Uri.encodeFull(url),
-          headers: {
-            "Accept": "application/json"
-          }
-      );
-    }catch(e){
-        print(e);
-    }
-
-
-    print(response);
-
-    if (response.statusCode == 200) {
-      print('http 200');
-      var jsonResponse = convert.jsonDecode(response.body);
-      var wineList = jsonResponse;
-      print(wineList);
-      print(wineList is List);
-
-      for (Map wine in wineList) {
-        print('shit');
-        wines.add(Wine(wine['wineName'], wine['wineImage']));
-      };
-      print('wines');
-      return wines;
-    }
-    else {
-      print('http 500');
-      print(response);
-    }
-    return wines;
-
-  }
 
   @override
   Widget build(BuildContext context) {
-    // return Center(child: Text('Search'));
-    return SafeArea(
-      child: buildFloatingSearchBar(),
-
-    );
-
-
-  }
-
-  Widget buildFloatingSearchBar() {
-    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return FloatingSearchBar(
-      hint: 'Search...',
-      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-      transitionDuration: const Duration(milliseconds: 800),
-      transitionCurve: Curves.easeInOut,
-      physics: const BouncingScrollPhysics(),
-      axisAlignment: isPortrait ? 0.0 : -1.0,
-      openAxisAlignment: 0.0,
-      width: isPortrait ? 600 : 500,
-      debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.place),
-            onPressed: () {},
-          ),
-        ),
-        FloatingSearchBarAction.searchToClear(
-          showIfClosed: false,
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: Colors.accents.map((color) {
-                return Container(height: 112, color: color);
-              }).toList(),
-            ),
-          ),
-        );
-      },
-    );
+    return SafeArea(child:  Center(
+        child: Container(
+            child: ListWidget())));
   }
 }
 
-//
-// class WineDetail extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: SafeArea(
-//         child: Column(
-//           children: <Widget>[
-//             IconButton(
-//               icon: Icon(Icons.arrow_back),
-//               onPressed: () => Navigator.of(context).pop(),
-//             ),
-//             Text("Detail"),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class ListWidget extends StatelessWidget {
+  final TextEditingController _textController = new TextEditingController();
+  final PostViewModel viewModel = PostViewModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Wine>>(
+      future: this.viewModel.searchByKeyword('amaranta'),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Column(
+            children: [
+              Container(
+                  margin: EdgeInsets.all(15.0),
+                  child: Row(
+                children: <Widget>[
+                  Flexible(
+                    child: TextField(
+                      controller: _textController,
+                      onSubmitted: _handleSubmitted,
+                      decoration: new InputDecoration.collapsed(
+                          hintText: "와인 이름으로 검색"),
+                    ),
+                  ),
+                  Container(
+                    // margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: () => _handleSubmitted(_textController.text)), ), ],)),
+              ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Image.network(snapshot.data[index].wineImageURL),
+                    title: Text('${snapshot.data[index].wineName}'),
+                    isThreeLine: false,
+                    subtitle: Text('${snapshot.data[index].wineName}'),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => WineDetail(wineItem: snapshot.data[index])));
+                    },
+                  );
+                },
+              )
+            ],
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // 기본적으로 로딩 Spinner를 보여줍니다.
+        return CircularProgressIndicator();
+      },
+    );
+  }
+  void _handleSubmitted(String text) { _textController.clear(); }
+
+
+}
