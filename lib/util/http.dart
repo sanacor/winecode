29532ac3ee1fe2f0522d91a39399db0685608fe1
sanha kang <ncsana@gmail.com>
2021-04-13 21:19:49 +0900

    앱아이콘 구현

--- lib/util/http.dart
+++ lib/util/http.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
-var BAKC_END_HOST = 'http://ec2-13-124-23-131.ap-northeast-2.compute.amazonaws.com:8080/';
+var BACK_END_HOST = 'http://ec2-13-124-23-131.ap-northeast-2.compute.amazonaws.com:8080/';
 
 
 Future<dynamic> http_get({header, String path}) async {
   try {
-    var response = await Dio().get(BAKC_END_HOST+path);
+    var response = await Dio().get(BACK_END_HOST+path);
     print(response);
     if (response.statusCode == 200) {
       return response.data;
@@ -20,7 +20,7 @@ Future<dynamic> http_get({header, String path}) async {
 // 추가 구현 필요
 Future<dynamic> http_post({header, String path, String body}) async {
   try {
-    var response = await Dio().post(BAKC_END_HOST+path);
+    var response = await Dio().post(BACK_END_HOST+path);
     print(response);
     if (response.statusCode == 200) {
       return response.data;
@@ -35,7 +35,7 @@ Future<dynamic> http_post({header, String path, String body}) async {
 // 추가 구현 필요
 Future<dynamic> http_delete(url, path, header) async {
   try {
-    var response = await Dio().delete(BAKC_END_HOST+path);
+    var response = await Dio().delete(BACK_END_HOST+path);
     print(response);
     if (response.statusCode == 200) {
       return response.data;
