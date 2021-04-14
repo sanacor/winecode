fcbb32d16a58de0fa6b449a843434e4e6b280c23
ooni0921 <ooni0921@gmail.com>
2021-04-14 15:44:03 +0900

    로그인 화면 깔끔하게 교체

--- lib/socialLogin/login_screen.dart
+++ lib/socialLogin/login_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/all.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:io' show Platform;
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 import 'dart:convert' show jsonEncode, utf8;
@@ -20,6 +22,60 @@ class _MyLoginPageState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+          child: Center(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(height: Platform.isAndroid ? 10 : 1 ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    alignment: Alignment.bottomLeft,
+                    child: RichText(
+                      text: TextSpan(
+                        text: '로그인',
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
+                      ),
+                    ),
+                    height: MediaQuery.of(context).size.height / 25,
+                  ),
+                  Padding(padding: const EdgeInsets.all(50)),
+                  Center(
+                    child: InkWell(
+                      onTap: _loginWithTalk,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*0.6,
+                        height: MediaQuery.of(context).size.height*0.07,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.yellow
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.chat_bubble, color: Colors.black54),
+                            SizedBox(width: 10),
+                            Text(
+                              '카카오톡으로 로그인',
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w900,
+                                fontSize: 20
+                              ),
+                            )
+                          ]
+                        )
+                      )
+                    )
+                  ),
+                ],
+              ),
+            )
+          )
+      ),
+    );
+    /*
+    return Scaffold(
       appBar: AppBar(title: const Text("Login")),
       body: Center(
         child: RaisedButton(
@@ -28,6 +84,7 @@ class _MyLoginPageState extends State<LoginScreen> {
         ),
       ),
     );
+    */
   }
 
   @override
@@ -56,12 +113,12 @@ class _MyLoginPageState extends State<LoginScreen> {
     try {
       var token = await AuthApi.instance.issueAccessToken(authCode);
       AccessTokenStore.instance.toStore(token);
-      print("AccessToken : " + token.accessToken);
+      //print("AccessToken : " + token.accessToken);
       await _registerUserInfoWithKakao(token.accessToken);
       await _loginWithKakao(token.accessToken);
       try {
         User user = await UserApi.instance.me();
-        print(user.toString());
+        //print(user.toString());
         Fluttertoast.showToast(
             msg: user.properties['nickname'] + "님 반갑습니다.",
             toastLength: Toast.LENGTH_SHORT,
