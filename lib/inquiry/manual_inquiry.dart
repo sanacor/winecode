7ce9902496a545de781b4ac48de29a362e0105c0
sanha kang <ncsana@gmail.com>
2021-04-17 22:47:44 +0900

    Manaul Inquiry: Manaul Inquiry UI 구현, back-end API 호출 구현 필요

--- lib/inquiry/manual_inquiry.dart
+++ lib/inquiry/manual_inquiry.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 class ManualInquiry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,20 +8,77 @@ class ManualInquiry extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Container(
-            child: Column(
+        child: Column(
+          children: [
+            Stack(
               children: [
                 Container(
-                  alignment: Alignment.bottomLeft,
+                    alignment: Alignment.bottomLeft,
                     child: IconButton(
                       icon: Icon(Icons.arrow_back, color: Colors.grey),
                       onPressed: () => Navigator.of(context).pop(),
                     )),
                 Container(
-                  height: MediaQuery.of(context).size.height / 25,
-                )
+                    padding: EdgeInsets.only(top: 15),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '와인 문의 하기',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    )),
+                Container(
+                    padding: EdgeInsets.only(top: 15, right: 15),
+                    alignment: Alignment.bottomRight,
+                    child: Text(
+                      '완료',
+                      style:
+                      TextStyle(fontSize: 17),
+                    )),
               ],
             ),
-          )),
+            const Divider(
+              height: 20,
+              thickness: 1,
+              indent: 0,
+              endIndent: 0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15, right: 15),
+              child: TextField(
+                obscureText: false,
+                decoration: InputDecoration(
+                    // border: OutlineInputBorder(),
+                    hintText: '와인 이름',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[300]),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey[300]),
+                  ),
+                ),),
+              ),
+            Container(
+              height: 200,
+              padding: EdgeInsets.only(left: 15, right: 15),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                obscureText: false,
+                decoration: InputDecoration(
+                  // border: OutlineInputBorder(),
+                  hintText: '문의하실 내용을 입력 해주세요:)',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x00000000)),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x00000000)),
+                  ),
+                ),),
+            ),
+
+          ],
+        ),
+      )),
     );
   }
 }
