4753485671b5c34448ad419630719c2d32bb0b13
sanha kang <ncsana@gmail.com>
2021-05-12 22:32:17 +0900

    답변하기 & 완료된 답변 화면 구분 기본 구조 구현

--- lib/reply/replyTile.dart
+++ lib/reply/replyTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wine/reply/replying.dart';
+import 'package:wine/reply/replied.dart';
 
 
 class ReplyTile extends StatelessWidget {
@@ -15,11 +16,11 @@ class ReplyTile extends StatelessWidget {
         child: Image.network('https://i.ibb.co/yQjhY5p/Screen-Shot-2021-04-15-at-10-52-11-PM.png', fit: BoxFit.fill),
       ),
       title: Text(_replyInfo.reply['inquery']['inqPdtName']),
-      // subtitle: Text("${_replyInfo.reply.toString()}"),
+      subtitle: Text("${_replyInfo.reply['reply']['rlyStatus']}"),
       // trailing: Text(_replyInfo.reply.toString()),
       onTap: () {
         Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => Replying()));
+            builder: (context) { return _replyInfo.reply['reply']['rlyStatus'] == 'Waiting' ? Replying(_replyInfo.reply) : Replied(_replyInfo.reply);}));
       },
     );
   }
