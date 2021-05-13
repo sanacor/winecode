cc4b9d8cc95961f6476f697798fbc702f126555d
ooni0921 <ooni0921@gmail.com>
2021-05-13 16:04:33 +0900

    이미지 업로드 구현

--- lib/testScreen/image_upload.dart
+++ lib/testScreen/image_upload.dart
@@ -1,5 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:http_parser/http_parser.dart';
 
 import 'package:multi_image_picker/multi_image_picker.dart';
 
@@ -62,6 +68,39 @@ class _UploadState extends State<ImageUploadScreen> {
     });
   }
 
+
+  _uploadImage() async {
+    if (images != null) {
+      // string to uri
+      Uri uri = Uri.parse(
+          'http://ec2-13-124-23-131.ap-northeast-2.compute.amazonaws.com:8080/api/image/upload');
+
+      // create multipart request
+      MultipartRequest request = http.MultipartRequest("POST", uri);
+
+      for (Asset image in images) {
+        ByteData byteData = await image.getByteData();
+        List<int> imageData = byteData.buffer.asUint8List();
+
+        MultipartFile multipartFile = MultipartFile.fromBytes(
+          'files', //key of the api
+          imageData,
+          //filename: 'some-file-name.jpg',
+          filename : image.name,
+          contentType: MediaType("image", "jpeg"),
+        );
+
+        // add file to multipart
+        request.files.add(multipartFile);
+      }
+
+      // send
+      var response = await request.send();
+      final respStr = await response.stream.bytesToString();
+      print(respStr);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -78,7 +117,7 @@ class _UploadState extends State<ImageUploadScreen> {
             ),
             RaisedButton(
               child: Text("Upload selected images"),
-              onPressed: loadAssets,
+              onPressed: _uploadImage,
             ),
             Expanded(
               child: buildGridView(),
