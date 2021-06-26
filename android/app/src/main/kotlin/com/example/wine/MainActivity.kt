1c577a2e298aa28685cb3bdd28625552ca57125a
sanha kang <ncsana@gmail.com>
2021-06-26 22:44:01 +0900

    출시전 com.example 패키지명 수정

--- android/app/src/main/kotlin/com/example/wine/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.wine
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/nclab/winefi/winecode/MainActivity.kt
+++ android/app/src/main/kotlin/nclab/winefi/winecode/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.winecode
+package nclab.winefi.winecode
 
 import io.flutter.embedding.android.FlutterActivity
 
