2b739d4834740822158cd44e6fe7d39d74b76cc1
sanha kang <ncsana@gmail.com>
2021-07-19 23:11:55 +0900

    와인샵 상세: 1. 와인샵 정보 ListView 형태가 아닌 여러개 ListTile으로 수정(스크롤 막음), 2.와인샵 와인 리스트 위젯 영역 정의

--- lib/map/wine_shop_detail.dart
+++ lib/map/wine_shop_detail.dart
@@ -3,11 +3,10 @@ import 'package:url_launcher/url_launcher.dart';
 import 'package:wine/map/model/wine_shop.dart';
 import 'dart:io' show Platform;
 
-
 class WineShopDetail extends StatefulWidget {
   final WineShop? wineShopItem;
 
-  const WineShopDetail ({ Key? key, this.wineShopItem }): super(key: key);
+  const WineShopDetail({Key? key, this.wineShopItem}) : super(key: key);
 
   @override
   _WineShopDetailState createState() => _WineShopDetailState();
@@ -16,7 +15,6 @@ class WineShopDetail extends StatefulWidget {
 class _WineShopDetailState extends State<WineShopDetail> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Center(
         child: Column(
@@ -27,8 +25,7 @@ class _WineShopDetailState extends State<WineShopDetail> {
                 child: IconButton(
                   icon: Icon(Icons.arrow_back, color: Colors.black),
                   onPressed: () => Navigator.of(context).pop(),
-                )
-            ),
+                )),
             Container(
               padding: const EdgeInsets.only(left: 20),
               alignment: Alignment.center,
@@ -36,18 +33,55 @@ class _WineShopDetailState extends State<WineShopDetail> {
                 text: TextSpan(
                   text: widget.wineShopItem!.retail_name!,
                   style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25),
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 25),
                 ),
               ),
               height: MediaQuery.of(context).size.height / 25,
             ),
             SizedBox(height: 30),
-            Expanded(
-                child: ListView(
-                  physics: BouncingScrollPhysics(),
-                  children: LoadSettingTile(context),
-                )
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text("전화번호"),
+              subtitle: Text(widget.wineShopItem!.retail_phone!),
+              onTap: () =>
+                  {launch("tel://" + widget.wineShopItem!.retail_phone!)},
             ),
+            ListTile(
+              leading: Icon(Icons.place),
+              title: Text("주소"),
+              subtitle: Text(widget.wineShopItem!.retail_address!),
+            ),
+            ListTile(
+              leading: Icon(Icons.schedule),
+              title: Text("영업시간"),
+              subtitle: Text(widget.wineShopItem!.retail_bhours! == ""
+                  ? "영업시간 정보없음"
+                  : widget.wineShopItem!.retail_bhours!),
+            ),
+            ListTile(
+              leading: Icon(Icons.description),
+              title: Text("가게 소개"),
+              subtitle: Text(widget.wineShopItem!.retail_exp! == ""
+                  ? widget.wineShopItem!.retail_address! +
+                      "에 위치한 " +
+                      widget.wineShopItem!.retail_name! +
+                      " 입니다."
+                  : widget.wineShopItem!.retail_exp!),
+            ),
+            SizedBox(height: 30),
+            Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: RichText(
+                    text: TextSpan(
+                        text: ("${widget.wineShopItem!.retail_name!}의 와인"),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 20))))
+            
           ],
         ),
       ),
@@ -60,9 +94,7 @@ class _WineShopDetailState extends State<WineShopDetail> {
       leading: Icon(Icons.call),
       title: Text("전화번호"),
       subtitle: Text(widget.wineShopItem!.retail_phone!),
-      onTap: () => {
-        launch("tel://" + widget.wineShopItem!.retail_phone!)
-      },
+      onTap: () => {launch("tel://" + widget.wineShopItem!.retail_phone!)},
     ));
     widgetList.add(ListTile(
       leading: Icon(Icons.place),
@@ -72,13 +104,19 @@ class _WineShopDetailState extends State<WineShopDetail> {
     widgetList.add(ListTile(
       leading: Icon(Icons.schedule),
       title: Text("영업시간"),
-      subtitle: Text(widget.wineShopItem!.retail_bhours! == "" ? "영업시간 정보없음" : widget.wineShopItem!.retail_bhours!),
+      subtitle: Text(widget.wineShopItem!.retail_bhours! == ""
+          ? "영업시간 정보없음"
+          : widget.wineShopItem!.retail_bhours!),
     ));
     widgetList.add(ListTile(
       leading: Icon(Icons.description),
       title: Text("가게 소개"),
-      subtitle: Text(widget.wineShopItem!.retail_exp! == "" ?
-        widget.wineShopItem!.retail_address! + "에 위치한 " + widget.wineShopItem!.retail_name! + " 입니다." : widget.wineShopItem!.retail_exp!),
+      subtitle: Text(widget.wineShopItem!.retail_exp! == ""
+          ? widget.wineShopItem!.retail_address! +
+              "에 위치한 " +
+              widget.wineShopItem!.retail_name! +
+              " 입니다."
+          : widget.wineShopItem!.retail_exp!),
     ));
     widgetList.add(ListTile(
       leading: Icon(Icons.local_offer),
